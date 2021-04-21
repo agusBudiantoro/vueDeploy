cbe162a5557e73779b77e95b4953872fea66a61a
Agus Budiantoro <agusbudiantoro97@gmail.com>
2021-04-21 10:56:11 +0700

    tambah db

---
+++ vue.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+function resolveSrc(_path) {
+  return path.join(__dirname, _path);
+}
+// vue.config.js
+module.exports = {
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Index Page',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    // when using the entry-only string format,
+    // template is inferred to be `public/subpage.html`
+    // and falls back to `public/index.html` if not found.
+    // Output filename is inferred to be `subpage.html`.
+    subpage: 'src/main.js'
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/production-sub-path/'
+    : '/',
+  // lintOnSave: true,
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  configureWebpack: {
+    // Set up all the aliases we use in our app.
+    resolve: {
+      alias: {
+        assets: resolveSrc('src/assets')
+      }
+    }
+  },
+  css: {
+    // Enable CSS source maps.
+    sourceMap: process.env.NODE_ENV !== 'production'
+  }
+};
